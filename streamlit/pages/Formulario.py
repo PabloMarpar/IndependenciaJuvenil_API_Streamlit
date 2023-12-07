453f52fd5175afa585a638898ebda96c5917665f
pablo <pablomartinpar@gmail.com>
2023-12-07 18:52:27 +0100

    gestion errores formulario

--- streamlit/pages/Formulario.py
+++ streamlit/pages/Formulario.py
@@ -6,8 +6,21 @@ if 'data_history' not in st.session_state:
     st.session_state.data_history = []
 
 # Formulario para datos de independencia
+
+# Función para validar si una cadena es un número entero
+def es_numero_entero(cadena):
+    return cadena.isdigit()
+
+# Formulario para datos de independencia
 año_independencia = st.text_input("Año de independencia:", key="año_independencia")
 edad_independencia = st.text_input("Edad al independizarse:", key="edad_independencia")
+
+# Validar si las entradas son números enteros
+if es_numero_entero(año_independencia) and es_numero_entero(edad_independencia):
+    año_independencia = int(año_independencia)
+    edad_independencia = int(edad_independencia)
+else:
+    st.write("Por favor, ingresa valores numéricos válidos para el año y la edad.")
 
 # Formulario para datos de hijos
 tiene_hijos = st.radio("¿Tienes hijos?", ["Sí", "No"], index=1)
@@ -19,14 +32,30 @@ if tiene_hijos == "Sí":
     for i in range(num_hijos):
         st.header(f"Datos del {i + 1}º hijo")
         año_hijo = st.text_input(f"Año del {i + 1}º hijo:", key=f"año_hijo_{i}")
+
+        # Validar si la entrada de año del hijo es un número entero
+        if es_numero_entero(año_hijo):
+            año_hijo = int(año_hijo)
+        else:
+            st.write(f"Por favor, ingresa un valor numérico válido para el año del {i + 1}º hijo.")
+
         edad_hijo = st.text_input(f"Edad al tener el {i + 1}º hijo:", key=f"edad_hijo_{i}")
+
+        # Validar si la entrada de edad del hijo es un número entero
+        if es_numero_entero(edad_hijo):
+            edad_hijo = int(edad_hijo)
+        else:
+            st.write(f"Por favor, ingresa un valor numérico válido para la edad del {i + 1}º hijo.")
+
         datos_hijos.append({f"Año_{i + 1}º": año_hijo, f"{i + 1}º": edad_hijo})
 
 else:
     datos_hijos = []
 
+
 # Botón para enviar los datos
 if st.button("Enviar Datos"):
+    
     # Crear diccionarios con los datos recopilados
     datos_independencia = {
         "España": año_independencia,
